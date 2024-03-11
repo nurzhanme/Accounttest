2dfb7c93900d45d22d05e69cff8a9e9313ed0670
Nurzhan Aitbayev <n-ait@mail.ru>
2024-03-11 13:09:35 +0600

    temp

---
+++ src/Web/Endpoints/Accounts.cs
@@ -0,0 +1,35 @@
+using CleanArchitecture.Application.Common.Models;
+using CleanArchitecture.Application.Accounts.Commands.CreateTodoAccount;
+using CleanArchitecture.Application.Accounts.Commands.FreezeOrUnfreezeAccount;
+using CleanArchitecture.Application.Accounts.Queries.GetTodoItemsWithPagination;
+
+namespace CleanArchitecture.Web.Endpoints;
+
+public class Accounts : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            //.RequireAuthorization()
+            .MapGet(GetTodoItemsWithPagination)
+            .MapPost(CreateAccount)
+            .MapPut(FreezeOrUnfreezeAccount, "{id}");
+    }
+
+    public Task<PaginatedList<AccountBriefDto>> GetTodoItemsWithPagination(ISender sender, [AsParameters] GetAccountsWithPaginationQuery query)
+    {
+        return sender.Send(query);
+    }
+
+    public Task<int> CreateAccount(ISender sender, CreateAccountCommand command)
+    {
+        return sender.Send(command);
+    }
+
+    public async Task<IResult> FreezeOrUnfreezeAccount(ISender sender, int id, FreezeOrUnfreezeAccountCommand command)
+    {
+        if (id != command.Id) return Results.BadRequest();
+        await sender.Send(command);
+        return Results.NoContent();
+    }
+}
